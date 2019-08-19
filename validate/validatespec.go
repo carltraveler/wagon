586e7cb03ad188ec5953c7df990f29ff4dfa02ba
steven <chenglin@ont.com>
2019-08-19 14:53:24 +0800

    add comment for newfile

--- validate/validatespec.go
+++ validate/validatespec.go
@@ -1,3 +1,8 @@
+// Copyright 2017 The go-interpreter Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// package validate provides functions for validating WebAssembly modules.
 package validate
 
 import (
